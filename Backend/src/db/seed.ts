45fcd6a44bea467f12da7d59b08a2a7099bf7687
vanshaj rana <vanshajrana@192.168.1.7>
2025-12-29 17:46:07 +0530

    set up prisma client

---
+++ Backend/src/db/seed.ts
@@ -0,0 +1,32 @@
+import { Sender } from "../../generated/prisma/index.js";
+import { prisma } from  "../db/prisma.js";
+
+async function seed(){
+    console.log("Seeding database...");
+
+    const conversation = await prisma.conversation.create({
+        data:{
+            messages: {
+                create : [
+                {
+                    sender : "user",
+                    text : "what is your return policy?" 
+                },
+                {
+                    sender : "ai",
+                    text : "Our return policy allows returns within 30 days of purchase with a valid receipt."
+                }
+                ]
+            }
+        }
+    });
+} 
+
+seed()
+    .catch((err)=>{
+        console.error("Seed failed")
+        process.exit(1)
+    })
+    .finally(async()=>{
+        await prisma.$disconnect();
+    })
