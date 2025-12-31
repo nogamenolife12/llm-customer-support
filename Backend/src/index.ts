f92bc51a6e1d4966ca6e2439309e31f7a9a7f8e8
vanshaj rana <vanshajrana@192.168.1.7>
2025-12-31 19:16:14 +0530

    Minor changes to error handling

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -17,7 +17,7 @@ app.use((err: any, req: any, res: any, next: any) => {
 });
 
 
-app.listen(PORT, () => {
+app.listen(Number(PORT), '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
