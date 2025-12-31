f6bea3258132cbdfde4f364b54f3768e9ed36767
vanshaj rana <vanshajrana@192.168.1.7>
2025-12-31 19:43:05 +0530

    debugging

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -4,7 +4,7 @@ const app = express();
 import chatRouter from "./routes/chat.route.js";
 
 app.use(cors({
-    origin: '*',
+    origin: ['http://localhost:5173/', 'https://llm-customer-support.onrender.com'],
     methods: ["GET", "POST"],
 }))
 app.use(express.json());
